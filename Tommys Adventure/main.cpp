738c42e864ad8765330baee1c1a5b0a1d9e060ee
Wiz1992 <itzmw3swag@gmail.com>
2019-01-04 17:23:31 +0100

    testing commiting

--- Tommys Adventure/main.cpp
+++ Tommys Adventure/main.cpp
@@ -3,4 +3,22 @@
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Tommy's Adventure");
+	sf::RectangleShape rectangle;
+	rectangle.setSize(sf::Vector2f(200.f, 200.f));
+	rectangle.setFillColor(sf::Color::Green);
+
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+			window.draw(rectangle);
+			window.display();
+
+
+		}
+
+	}
 }
