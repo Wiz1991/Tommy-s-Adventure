0d1ac9968dcc61b707f5379d484ca0012b164d62
Wiz1991 <45233935+Wiz1991@users.noreply.github.com>
2019-02-01 09:35:24 +0100

    Update StateManager.cpp

--- Tommys Adventure/StateManager.cpp
+++ Tommys Adventure/StateManager.cpp
@@ -1,12 +1,19 @@
 #include "StateManager.h"
-
+#include <SFML/Graphics.hpp>
+StateManager::StateManager() : mCurrentStateIndetifier(static_cast<int>(State::States::INTRO))
+{}
 
 
 void StateManager::RegisterStates(){
  for(int i=0;i<static_cast<int>(State::States::TOTAL);i++){
-   
-   
+   mStateStack.push_back(getStateFromType(static_cast<State::States>(i)));
  }
-  
-  
 }
+
+void StateManager::Update(sf::Time dT){
+   mStateStack[mCurrentStateIndetifier]->Update();
+   if(mCurrentState!=mNextState)
+    mCurrentState
+ 
+}
+
